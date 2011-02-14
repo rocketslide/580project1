826e3f01363143bb8b643c1d540eca889991e500
agoliak <agoliak@gmail.com>
2011-02-13 23:20:32 -0600

    Final? probably not

---
+++ project1.js
@@ -0,0 +1,22 @@
+
+            $(document).ready(function() {
+            $('.datepicker input#dateworn').datepicker();
+            });
+        
+         $(document).ready(function() {
+    $("input#brand").autocomplete({
+    source: ["Dr Martins", "John Fluevog", "Frye", "BC Footwear"]
+});
+  });
+  
+         $(document).ready(function() {
+    $("input#material").autocomplete({
+    source: ["Leather", "Patent", "Fabric"]
+});
+  });
+  
+         $(document).ready(function() {
+    $("input#mood").autocomplete({
+    source: ["Bad ass", "Sexy as hell", "Stompy", "Slick"]
+});
+  });
